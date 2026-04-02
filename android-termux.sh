302a80b1953c2b529113773ce22ad8efb2c6830e
Kir Semenov <kirsem11@yandex.ru>
2026-04-02 11:07:53 +0300

    Update an easy-install bash script for termux (android)

--- android-termux.sh
+++ android-termux.sh
@@ -1,20 +1,22 @@
 set -e
 
 # constants
-SECRET="71aff5b77c58c1f13efa5e48ded5acf3" # without 'dd' prefix
-TG_WS_PROXY_RUN_CMD="python ~/tg-ws-proxy/proxy/tg_ws_proxy.py --secret $SECRET --dc-ip 5:149.154.175.55"
+REPO_NAME="tg-ws-proxy"
+REPO_ROOT="~/"
 BASHRC="~/.bashrc"
+
+RUN_CMD="pgrep -f "tg_ws_proxy.py" > /dev/null || nohup python $REPO_ROOT/$REPO_NAME/proxy/tg_ws_proxy.py --secret $SECRET --dc-ip 5:149.154.175.55 &"
+SECRET="71aff5b77c58c1f13efa5e48ded5acf3" # without 'dd' prefix # this is random secret, you can use it or generate your own MTProxy secret
 
 # termux update & upgrade & install requirement packages
 pkg update && pkg upgrade -y -o Dpkg::Options::="--force-confold"
 pkg install -y git python python-pip python-cryptography
 
 # cloning into ~/ directory
-git clone https://github.com/Flowseal/tg-ws-proxy && cd tg-ws-proxy
-
-# start proxy
-$TG_WS_PROXY_RUN_CMD
-echo "Proxy started!"
+cd $REPO_ROOT && git clone "https://github.com/Flowseal/$REPO_NAME"
 
 # set autocommand in $BASHRC
-echo "$TG_WS_PROXY_RUN_CMD" >> $BASHRC
+echo "$RUN_CMD" >> $BASHRC
+
+# start proxy
+$RUN_CMD
