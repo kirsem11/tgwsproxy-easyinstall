66e0da7bc5e6b15a3f5cf16579132f4943e13cf7
Kir Semenov <kirsem11@yandex.ru>
2026-04-02 11:48:24 +0300

    Update an easy-install bash script for termux (android)

--- android-termux.sh
+++ android-termux.sh
@@ -11,9 +11,12 @@ REPO_NAME="tg-ws-proxy"
 REPO_ROOT="$HOME"
 BASHRC="$HOME/.bashrc"
 
-CMD="tg_ws_proxy.py"
-RUN_CMD="pgrep -f '$CMD' > /dev/null || nohup python $REPO_ROOT/$REPO_NAME/proxy/tg_ws_proxy.py --secret $SECRET --dc-ip 5:149.154.175.55 &"
 SECRET="71aff5b77c58c1f13efa5e48ded5acf3" # without 'dd' prefix # this is random secret, you can use it or generate your own MTProxy secret
+PORT=1443
+DC_IP="--dc-ip 5:149.154.175.55" # you can specify more DC IPs
+
+CMD="tg_ws_proxy.py"
+RUN_CMD="pgrep -f '$CMD' > /dev/null || nohup python $REPO_ROOT/$REPO_NAME/proxy/tg_ws_proxy.py --port $PORT --secret $SECRET $DC_IP &"
 
 # termux update & upgrade & install requirement packages
 pkg update && pkg upgrade -y -o Dpkg::Options::="--force-confold"
