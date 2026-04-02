5763c44c662bba423e63a263fceb97b7e58227f3
Kir Semenov <kirsem11@yandex.ru>
2026-04-02 13:14:06 +0300

    Update an easy-install bash script for termux (android)

--- android-termux.sh
+++ android-termux.sh
@@ -4,7 +4,7 @@
 # Copyrigth (c) 2026 Kirill Semenov
 ##################################################
 
-set -e
+set -ex
 
 # constants
 REPO_NAME="tg-ws-proxy"
@@ -14,7 +14,14 @@ BASHRC="$HOME/.bashrc"
 # this is random secret, you can use it or generate your own MTProto proxy secret
 SECRET="71aff5b77c58c1f13efa5e48ded5acf3" # without 'dd' prefix
 PORT=1443
-DC_IP="--dc-ip 1:149.154.175.50 --dc-ip 2:149.154.167.220 --dc-ip 3:149.154.175.100 --dc-ip 4:149.154.167.220 --dc-ip 5:149.154.171.5 --dc-ip 5:149.154.175.55" # you can specify more DC IPs
+DC_IP="
+--dc-ip 1:149.154.175.50
+--dc-ip 2:149.154.167.220
+--dc-ip 3:149.154.175.100
+--dc-ip 4:149.154.167.220
+--dc-ip 5:149.154.171.5
+--dc-ip 5:149.154.175.55
+--dc-ip 203:91.105.192.100"
 
 CMD="tg_ws_proxy.py"
 
@@ -22,7 +29,7 @@ CMD="tg_ws_proxy.py"
 run_proxy( )
 {
   if ! pgrep -f "$CMD" > /dev/null; then
-    nohup python $REPO_ROOT/$REPO_NAME/proxy/tg_ws_proxy.py --port $PORT --secret $SECRET $DC_IP2 >&1 | tee nohup.log &
+    nohup python $REPO_ROOT/$REPO_NAME/proxy/tg_ws_proxy.py -v --port $PORT --secret $SECRET $DC_IP 2>&1 | tee nohup.log &
     echo "The proxy server is started"
   else
     echo "The proxy server is already running"
@@ -41,7 +48,13 @@ pkg update && pkg upgrade -y -o Dpkg::Options::="--force-confold"
 pkg install -y git python python-pip python-cryptography
 
 # cloning repo
-git clone "https://github.com/Flowseal/$REPO_NAME" "$REPO_ROOT/$REPO_NAME"
+if [ -d "$REPO_ROOT/$REPO_NAME" ]; then
+    echo "Update repository..."
+    cd "$REPO_ROOT/$REPO_NAME" && git pull
+else
+    echo "Clone repository..."
+    git clone "https://github.com/Flowseal/$REPO_NAME" "$REPO_ROOT/$REPO_NAME"
+fi
 
 # start
 update_bashrc
